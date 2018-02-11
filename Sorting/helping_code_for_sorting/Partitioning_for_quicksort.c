1d2f918ded9d371b0476f79087818a4f2c41c74a
sreekar2307 <bollamsreekhar@gmail.com>
2018-02-11 15:50:20 +0530

    added Partioning Code of quick Sort

---
+++ Sorting/helping_code_for_sorting/Partitioning_for_quicksort.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int A[] = {20,11,7,5,12,6,15,13};
+  int pointer_r=0,pointer_s=0,pointer_t=sizeof(A)/sizeof(int),pivot=20;
+  int flag,count=1;
+  for(int i=7;i>=0;i--)
+  {
+      if(A[i]>pivot)
+        pointer_t--;
+      else
+        break;
+  }
+  while(pointer_s<pointer_t){
+    if(A[pointer_s]==pivot)
+   {
+      pointer_s++;
+   }
+    else if(pivot>A[pointer_s])
+    {
+        if(count){
+        flag=A[pointer_s];
+        A[pointer_s]=A[pointer_r];
+        A[pointer_r]=flag;count--;
+        }
+       else{
+        pointer_r++;
+        flag=A[pointer_s];
+        A[pointer_s]=A[pointer_r];
+        A[pointer_r]=flag;
+        }
+        pointer_s++;
+    }
+    else
+   {
+        if(pointer_t==sizeof(A)/sizeof(int))
+      {
+        flag=A[pointer_s];
+        A[pointer_s]=A[pointer_t-1];
+        A[pointer_t-1]=flag;
+        pointer_t--;
+      }
+        else
+      {
+        pointer_t--;
+        flag=A[pointer_s];
+        A[pointer_s]=A[pointer_t];
+        A[pointer_t]=flag;
+      }
+   }
+   printf("%d %d %d\n",pointer_r,pointer_s,pointer_t);
+  }
+  printf("\n");
+  for(int i=0;i<sizeof(A)/sizeof(int);i++)
+    printf("%d ",A[i]);
+  return 0;
+}
