ac10447bc09128c3127f5e6b40cb3cddfe6da1a0
Sreekar2307 <bollamsreekhar@gmail.com>
2018-05-26 13:11:31 +0530

    DFS which prints result of traversal

--- graph/Depth_first_search.java
+++ graph/Depth_first_search.java
@@ -9,6 +9,7 @@ public class Depth_first_search {
 		 graph_node vertices[] = new graph_node[no_of_nodes];
 		 for(int i=0;i<no_of_nodes;i++)
 			 vertices[i]= new graph_node(i);
+		 int DFS[] = new int[no_of_nodes];
 		 vertices[0].add(1,1);
 		 vertices[0].add(1,4);
 		 vertices[1].add(1,0);
@@ -30,20 +31,24 @@ public class Depth_first_search {
 		 
 		 System.out.println("Select source Node");
 		 int source = scan.nextInt();
-		 
-		 int i=0;
-		 depth_First_search(source,vertices,no_of_nodes);
+		 int i=0,index=0;
+		 depth_First_search(DFS,source,vertices,no_of_nodes,index);
 		 while(i<no_of_nodes-1)
 		 {
 			 if(!vertices[i].color) {
 				 System.out.printf("Detected non_connected component\n");
-				 depth_First_search(i,vertices,no_of_nodes);
+				 depth_First_search(DFS,i,vertices,no_of_nodes,index);
 			 }
 			 i++;
 		 }
+		 System.out.println();
+		 for(i=0;i<no_of_nodes;i++) 
+			 System.out.print(DFS[i]+" ");
+			 
 }
-	static void depth_First_search(int v,graph_node A[],int no_of_nodes){
+	static void depth_First_search(int DFS[],int v,graph_node A[],int no_of_nodes,int index){
 		time++;
+		DFS[index++]=v;
         A[v].color=true;
 		A[v].in_time=time;
 		int k;
@@ -52,7 +57,7 @@ public class Depth_first_search {
 			if(!A[A[v].ajacent_edges.get(k).vertex_no].color)
 			 {
 				A[A[v].ajacent_edges.get(k).vertex_no].vertex_parent=v;
-				depth_First_search(A[v].ajacent_edges.get(k).vertex_no,A,no_of_nodes);
+				depth_First_search(DFS,A[v].ajacent_edges.get(k).vertex_no,A,no_of_nodes,index);
 			 }
 			else if(A[v].vertex_parent!=A[v].ajacent_edges.get(k).vertex_no && A[A[v].ajacent_edges.get(k).vertex_no].out_time==0) {
 				System.out.println("back edge from "+v+" to "+A[v].ajacent_edges.get(k).vertex_no);
